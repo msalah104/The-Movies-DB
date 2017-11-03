1341dedc73304098f27dcafe8f562a27576f47fe
Mohamed Salah <mohamed.salah.2011@gmail.com>
2017-11-03 23:37:08 +0200

    Initial Commit

---
+++ The Movies DB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  The Movies DB
+//
+//  Created by Mohamed Salah on 11/3/17.
+//  Copyright © 2017 Mohamed Salah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
